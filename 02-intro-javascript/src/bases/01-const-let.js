77eb1d47483c30a7b3b8d3948acd97779184756b
Mattullon <mattullon@gmail.com>
2022-12-19 08:33:39 -0300

    inicio de curso

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,9 @@
+console.log("hola mundo ")
+
+const nombre= 'matias '
+let apellido='ullon'
+console.log(nombre,apellido)
+if(true){
+    let apellido='dadario'
+    console.log(apellido)
+}
---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,9 @@
+
+
+const nombre ='matias '
+const apellido='ullon'
+
+function nombreCompleto(nombre){
+    return 'hola '+ nombre
+}
+console.log(`este es mi nombre : ${apellido} ${nombreCompleto(nombre)}`)
