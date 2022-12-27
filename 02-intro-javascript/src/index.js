47eb31fafa6ea0ed053753ec6fa97dc89ca7fafe
Mattullon <mattullon@gmail.com>
2022-12-27 12:12:16 -0300

    s

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,3 +1,47 @@
  
 
- const apiKey = 'FvdTNoVLGsOr3ns63QjxVxvDuqLotKo8'
+//  const getImagenPromesa = ()=>
+//   new Promise(  resolve => resolve('http:asadadds.com'))
+    
+ 
+ //getImagenPromesa().then(console.log)
+
+ //async al ponerle async automaticamente podes 
+ // usar las propiedades de una promesa 
+  
+ 
+    const getImagen = async()=>{
+    
+        const apiKey = 'FvdTNoVLGsOr3ns63QjxVxvDuqLotKo8'
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+        
+        
+        const {data} = await resp.json();
+        const {url}= data.images.original;
+        console.log(resp)
+        const img = document.createElement('img');
+        img.src=url;
+        document.body.append(img);
+        }
+getImagen()
+
+       
+
+  
+  
+  
+  
+
+
+
+//  peticion
+//     .then( resp => resp.json())
+//     .then( ({data}) => {
+//         const {url}= data.images.original
+        
+//         const img = document.createElement('img');
+//         img.src=url;
+//         document.body.append(img);
+
+//     })
+//     .catch(console.warn);
